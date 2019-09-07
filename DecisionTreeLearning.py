f0179cebee2871d88893e658edde01c8d6213350
Prasad Rao <prasad.rao@gatech.edu>
2019-09-07 14:47:34 +0530

    Decision Tree: Beginning initial class structure

---
+++ DecisionTreeLearning.py
@@ -0,0 +1,13 @@
+
+class decisionTreeLearner():
+    def __init__(self):
+        return
+
+    def evaluateDataCharacteristics(self):
+        return
+
+    def loadData(self, pathToData):
+        return;
+
+if __name__ == '__main__':
+    dtLearner = decisionTreeLearner()
